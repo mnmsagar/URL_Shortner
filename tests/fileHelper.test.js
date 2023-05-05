4beb0415f4201a759240e1da92b82b65d8baf109
mnmsagar <54708901+mnmsagar@users.noreply.github.com>
2023-05-05 16:01:51 +0530

    Delete fileHelper.test.js

--- tests/fileHelper.test.js
+++
@@ -1,6 +0,0 @@
-const fileHelper = require('../services/fileHelper');
-
-test("Promise Test",()=>{
-    let arr = [];
-    return expect(fileHelper.writeFileAsync(arr)).resolves.toBe("File written Successfully");
-})
