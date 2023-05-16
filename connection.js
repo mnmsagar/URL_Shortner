f83148ebd27e58bea9a1d9512e294b6ac5e95138
Sagar <sagar785mishra@gmail.com>
2023-05-16 19:01:57 +0530

    refactor: changed database connection from a callback to promise for better working and usage

--- connection.js
+++ connection.js
@@ -2,18 +2,9 @@ const { MongoClient } = require('mongodb');
 
 let dbConnection;
 module.exports = {
-    connectToDb : (cb)=>{
-        const url = process.env.MONGO_URL;
-        MongoClient.connect('mongodb://0.0.0.0:27017/url')
-        .then((client)=>{
-            dbConnection = client.db();
-            
-            return cb()
-        })
-        .catch(err => { 
-            console.log(err);
-            return cb(err)
-        })
+    connectToDb : async (dbUrl)=>{
+       const client =  await MongoClient.connect(dbUrl);
+       dbConnection = client.db('url');
     },
     getDb : ()=> dbConnection
 }
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const { port, app } = require("./app");
 const { connectToDb } = require("./connection");
 
-
-connectToDb((err) => {
-	if (!err) {
+connectToDb(process.env.URI).then(() => {
+	try {
 		app.listen(port, () => {
-			console.log(`Server is running on port number - ${port}`);
+			console.log(`Server is running on - ${port}`);
 		})
+	} catch (error) {
+		console.log("Can't connect to the server!");
 	}
+}).catch((error) => {
+	console.log("Invalid Database Connection");
 })
 
 
