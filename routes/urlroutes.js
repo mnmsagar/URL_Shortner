797eb040417a3d87d0fa09d989de8b3f9dd1ab1a
Sagar <sagar785mishra@gmail.com>
2023-05-17 17:22:45 +0530

    feat: added middleware for authentication for post route

---
+++ routes/urlroutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { addURL, getLink} = require("../controllers/urlControllers");
+const {checkUserAuth} = require("../auth-middleware");
+
+const router = express.Router();
+
+router.use("/shorten",checkUserAuth);
+
+router.route("/:id").get(getLink);
+router.route("/shorten").post(addURL);
+
+
+module.exports = {
+	router
+};
