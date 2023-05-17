637d0e22e377626826d5f7c5c4d80295d7f1cf9e
Sagar <sagar785mishra@gmail.com>
2023-05-17 17:21:03 +0530

    fix : fixing addURL and getObjById

--- services/dataHelper.js
+++ services/dataHelper.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
-// const data = JSON.parse(fs.readFileSync(`${__dirname}/../models/data.json`));
-
-const dbConnect = require("../connection.js");
 const randomstring = require("randomstring");
 const { writeFileAsync } = require("./fileHelper");
 const isUrlHttp = require("is-url-http");
+const {getDb} = require("../connection");
+
+
 
 module.exports = {
 	writeToFile: async (url) => {
-		const id = await randomstring.generate(8);
+		const id = randomstring.generate(8);
 		const obj = {
 			urlCode: id,
 			longUrl: url,
@@ -17,23 +17,20 @@ module.exports = {
 		const newObj = {
 			...obj
 		}
-		let data = await dbConnect();
-		await data.insertOne(obj);
+		await getDb().collection('urlshortner').insertOne(obj);
 		return newObj;
-		
+		 
 	},
-	getObjById: async (id) => {
-		const data = await dbConnect();
-		const obj = data.findOne({ urlCode: id });
+	getObjById:  async (id) => {
+		const obj =  await getDb().collection('urlshortner').findOne({urlCode : id});
 		return obj;
-
 	},
-
+	
 	checkBodyandURL: (body) => {
 		if (body.url) {
 			let isValidURl = isUrlHttp(body.url);
 			return isValidURl;
 		}
 		return false;
-	},
-};
+	}
+}
