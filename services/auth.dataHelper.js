a6589cdc2a4ff29bdaf9b948d826a3982e7f324f
Sagar <sagar785mishra@gmail.com>
2023-05-17 17:10:53 +0530

    refactor: checkUserAuth refactored

---
+++ services/auth.dataHelper.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const jwt = require('jsonwebtoken');
+const { getDb } = require("../connection");
+const { ObjectId } = require("mongodb");
+
+exports.auth = async (authorization)=>{
+    const token = authorization.split(' ')[1];
+    const {userID, email} = jwt.verify(token, process.env.SECRET_KEY);
+    const obj = await getDb().collection('users').findOne({_id : new ObjectId(userID)});
+    return obj;
+}
