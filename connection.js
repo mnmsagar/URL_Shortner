e085a32ad4f03cace53559f72832db38db02b2b7
Sagar <sagar785mishra@gmail.com>
2023-05-18 18:29:45 +0530

    Refactor: ♻️ Reorganize file structure and update imports for better code organization

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, Timestamp } = require('mongodb');
 
 let dbConnection;
 module.exports = {
     connectToDb : async (dbUrl)=>{
        const client =  await MongoClient.connect(dbUrl);
+       console.log("Connected to Database");
        dbConnection = client.db('url');
     },
     getDb : ()=> dbConnection
