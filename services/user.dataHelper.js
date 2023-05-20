424ece790fe6f25534c80ad6a98762f9632bdd64
Sagar <sagar785mishra@gmail.com>
2023-05-20 11:48:16 +0530

    refactor: added methods for validations of email, password and name

--- services/user.dataHelper.js
+++ services/user.dataHelper.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
 const { getDb } = require('../connection');
 const jwt = require('jsonwebtoken');
-const { hash, compare } = require('bcrypt');
 const { validate } = require('email-validator');
-const  passwordValidator = require('password-validator');
+const passwordValidator = require('password-validator');
+const crypto = require('crypto');
 
 const schema = new passwordValidator();
 schema
-.is().min(8)                                    // Minimum length 8
-.is().max(100)                                  // Maximum length 100
-.has().uppercase()                              // Must have uppercase letters
-.has().lowercase()                              // Must have lowercase letters
-.has().digits(2)                                // Must have at least 2 digits
-.has().not().spaces()                           // Should not have spaces
+    .is().min(8)                                    // Minimum length 8
+    .is().max(100)                                  // Maximum length 100
+    .has().uppercase()                              // Must have uppercase letters
+    .has().lowercase()                              // Must have lowercase letters
+    .has().digits(2)                                // Must have at least 2 digits
+    .has().not().spaces()                           // Should not have spaces
 
 
-const isValidString =(str)=> {
+const isValidString = (str) => {
     if (str.trim().length === 0) {
         return false;
     }
@@ -27,7 +27,7 @@ const isValidString =(str)=> {
 
 exports.addUserHelper = async (body) => {
     const { email, password, name } = body
-    const hashedPassword = await hash(password, 10);
+    const hashedPassword = hashPassword(password);
     const user = {
         email: email,
         name: name,
@@ -44,38 +44,74 @@ exports.existUser = async (email) => {
     return obj;
 }
 
-exports.matchPass = async (password, existingUser) => {
-    const obj = await compare(password, existingUser.password);
-    return obj;
-}
+// exports.matchPass = async (password, existingUser) => {
+//     const obj = await compare(password, existingUser.password);
+//     return obj;
+// }
 
 exports.tokenGeneration = (obj) => {
     const token = jwt.sign({ email: obj.email, userID: obj._id }, process.env.SECRET_KEY);
     return token;
 }
 
-exports.checkBody = async (body) => {
+exports.checkBody = (body) => {
     const { name, email, password } = body;
-    if(isValidString(name)){
-        if(validate(email)){
-            if(schema.validate(password)){
+    if (isValidString(name)) {
+        if (validate(email)) {
+            if (schema.validate(password)) {
                 return true;
-            }else{
+            } else {
                 return {
-                    message : "Invalid password, Password should contains : min length of 8 characters, max of 100 characters, Must have uppercase letters, must have lowercase letters, must have atleasr 2 digits, should not have any spaces",
-                    statusCode : 404
-                } 
+                    message: "Invalid password, Password should contains : min length of 8 characters, max of 100 characters, Must have uppercase letters, must have lowercase letters, must have atleast 2 digits, should not have any spaces",
+                    statusCode: 404
+                }
             }
-        }else{
+        } else {
             return {
-                message : "Invalid email, Please type correct email",
-                statusCode : 404
+                message: "Invalid email, Please type correct email",
+                statusCode: 404
             }
         }
-    }else{
+    } else {
         return {
-            message : "Invalid name, Please use characters only",
-            statusCode : 404
+            message: "Invalid name, Please use characters only",
+            statusCode: 404
         }
     }
+}
+
+exports.existingUserAndPasswordCheck = async (email, password) => {
+    const obj = await getDb().collection('users').findOne({ email: email });
+    if (!obj) {
+        return {
+            message: "Incorrect Credentials"
+        }
+    } else {
+        if (verifyPassword(password, obj.password)) {
+            return {
+                status: 'Success',
+                user: obj
+            };
+        } else {
+            return { message: "Incorrect Credentials" }
+        }
+    }
+}
+
+const hashPassword = (password) => {
+    const hmac = crypto.createHmac('sha512', process.env.HASH_KEY);
+    hmac.update(password);
+    const passwordHash = hmac.digest('hex');
+    return passwordHash;
+}
+
+const verifyPassword = (password, storedHash) => {
+    const hmac = crypto.createHmac('sha512', process.env.HASH_KEY);
+    hmac.update(password);
+    const passwordHash = hmac.digest('hex');
+    return passwordHash === storedHash;
+}
+
+exports.isEmailValid = (email) => {
+    return validate(email);
 }
