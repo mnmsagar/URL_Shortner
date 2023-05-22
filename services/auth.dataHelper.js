b24ef693e1eb9a68bde41346019ad1582ee2ed4f
Sagar <sagar785mishra@gmail.com>
2023-05-22 12:47:43 +0530

    refactor: deleted unused variables

--- services/auth.dataHelper.js
+++ services/auth.dataHelper.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const { getDb } = require("../connection");
 const { ObjectId } = require("mongodb");
 
-exports.auth = async (authorization)=>{
-    const token = authorization.split(' ')[1];
-    const {userID, email} = jwt.verify(token, process.env.SECRET_KEY);
-    const obj = await getDb().collection('users').findOne({_id : new ObjectId(userID)});
-    return obj;
-}
+exports.auth = async (authorization) => {
+	const token = authorization.split(" ")[1];
+	const { userID } = jwt.verify(token, process.env.SECRET_KEY);
+	const obj = await getDb()
+		.collection("users")
+		.findOne({ _id: new ObjectId(userID) });
+	return obj;
+};
