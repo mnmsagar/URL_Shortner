f53089090d377ea772b4225edced5359e444cb73
Sagar <sagar785mishra@gmail.com>
2023-05-22 12:46:45 +0530

    refactor: refactored userAndPasswordCheck function

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,52 +1,58 @@
 require("dotenv").config();
-const { addUserHelper, existUser, tokenGeneration, checkBody, userAndPasswordCheck } = require("../services/user.dataHelper");
+const {
+	addUserHelper,
+	existUser,
+	tokenGeneration,
+	checkBody,
+	userAndPasswordCheck,
+} = require("../services/user.dataHelper");
 const { isValidEmail } = require("../utils/utils");
 
 exports.addUser = async (req, res) => {
-    const { email, password, name } = req.body;
-    const response = checkBody(req.body);
-    if (response === true) {
-        const existingUser = await existUser(email);
-        if (existingUser) {
-            res.status(409).json({
-                message: "User already Exists"
-            })
-            return;
-        }
-        const obj = await addUserHelper(req.body);
-        const token = tokenGeneration(obj);
-        res.status(201).json({
-            status: "success",
-            hint: "successfully registered",
-            token: token
-        })
-    } else {
-        res.status(response.statusCode).json(response.message);
-    }
-}
+	const { email } = req.body;
+	const response = checkBody(req.body);
+	if (response === true) {
+		const existingUser = await existUser(email);
+		if (existingUser) {
+			res.status(409).json({
+				message: "User already Exists",
+			});
+			return;
+		}
+		const obj = await addUserHelper(req.body);
+		const token = tokenGeneration(obj);
+		res.status(201).json({
+			status: "success",
+			hint: "successfully registered",
+			token: token,
+		});
+	} else {
+		res.status(response.statusCode).json(response.message);
+	}
+};
 
 exports.userlogin = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        if (!(isValidEmail(email))) {
-            res.status(400).json({
-                status: "failed",
-                message: "Invalid email type"
-            })
-            return;
-        }
-        const obj = await userAndPasswordCheck(email, password);
-        if (!(obj.status)) {
-            res.status(401).json(obj);
-            return;
-        }
-        const token = tokenGeneration(obj.user);
-        res.status(200).json({ message: "Logged In Successfully", token: token });
-    }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: "something went wrong"
-        })
-    }
-}
+	const { email, password } = req.body;
+	try {
+		if (!isValidEmail(email)) {
+			res.status(400).json({
+				status: "failed",
+				message: "Invalid email type",
+			});
+			return;
+		}
+		const obj = await userAndPasswordCheck(email, password);
+		if (obj.message) {
+			res.status(401).json(obj);
+			return;
+		}
+		console.log(obj);
+		const token = tokenGeneration(obj);
+		res.status(200).json({ message: "Logged In Successfully", token: token });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({
+			message: "something went wrong",
+		});
+	}
+};
--- services/user.dataHelper.js
+++ services/user.dataHelper.js
@@ -1,68 +1,65 @@
 require("dotenv").config();
-const { getDb } = require('../connection');
-const jwt = require('jsonwebtoken');
-const { isValidPassword, isValidString, isValidEmail, hashPassword } = require('../utils/utils');
+const { getDb } = require("../connection");
+const jwt = require("jsonwebtoken");
+const { isValidPassword, isValidString, isValidEmail, hashPassword } = require("../utils/utils");
 
 exports.addUserHelper = async (body) => {
-    const { email, password, name } = body
-    const hashedPassword = hashPassword(password);
-    const user = {
-        email: email,
-        name: name,
-        password: hashedPassword
-    }
-    const obj = user;
-    const newObj = { ...obj };
-    await getDb().collection('users').insertOne(obj);
-    return obj;
-}
+	const { email, password, name } = body;
+	const hashedPassword = hashPassword(password);
+	const user = {
+		email: email,
+		name: name,
+		password: hashedPassword,
+	};
+	const obj = user;
+	// const newObj = { ...obj };
+	await getDb().collection("users").insertOne(obj);
+	delete obj._id;
+	return obj;
+};
 
 exports.existUser = async (email) => {
-    const obj = await getDb().collection('users').findOne({ email: email });
-    return obj;
-}
+	const obj = await getDb().collection("users").findOne({ email: email });
+	return obj;
+};
 
 exports.tokenGeneration = (obj) => {
-    const token = jwt.sign({ email: obj.email, userID: obj._id }, process.env.SECRET_KEY);
-    return token;
-}
+	const token = jwt.sign({ email: obj.email, userID: obj._id }, process.env.SECRET_KEY);
+	return token;
+};
 
 exports.checkBody = (body) => {
-    const { name, email, password } = body;
-    if (!(isValidString(name))) {
-        return {
-            message: "Invalid name, Please use characters only",
-            statusCode: 400
-        }
-    }
-    if (!(isValidEmail(email))) {
-        return {
-            message: "Invalid email, Please type correct email",
-            statusCode: 400
-        }
-    }
-    if (!(isValidPassword(password))) {
-        return {
-            message: "Invalid password, Password should contains : min length of 8 characters, max of 100 characters, Must have uppercase letters, must have lowercase letters, must have atleast 2 digits, should not have any spaces",
-            statusCode: 400
-        }
-    }
-    return true;
-}
+	const { name, email, password } = body;
+	if (!isValidString(name)) {
+		return {
+			message: "Invalid name, Please use characters only",
+			statusCode: 400,
+		};
+	}
+	if (!isValidEmail(email)) {
+		return {
+			message: "Invalid email, Please type correct email",
+			statusCode: 400,
+		};
+	}
+	if (!isValidPassword(password)) {
+		return {
+			message:
+				"Invalid password, Password should contains : min length of 8 characters, max of 100 characters, Must have uppercase letters, must have lowercase letters, must have atleast 2 digits, should not have any spaces",
+			statusCode: 400,
+		};
+	}
+	return true;
+};
 
 exports.userAndPasswordCheck = async (email, password) => {
-    const result = await getDb().collection('users').findOne({ email: email });
-    if (!(result)) {
-        return {
-            message: "Incorrect Credentials"
-        }
-    }
-    if (!(result.password === hashPassword(password))) {
-        return { message: "Incorrect Credentials" }
-    }
-    return {
-        status: 'Success',
-        user: result
-    }
-}
-
+	password = hashPassword(password);
+	const result = await getDb().collection("users").findOne({ email: email, password: password }, { email: 1 });
+	if (!result) {
+		return {
+			message: "Invalid Credentials",
+		};
+	}
+	console.log(result);
+	return result;
+};
