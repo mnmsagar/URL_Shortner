97211bedcfe8d0cde6cb70e31da9d6854dec9722
Sagar <sagar785mishra@gmail.com>
2023-05-22 20:22:28 +0530

    add: forgotPassword, resetPassword and verify-email routes added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const {addUser, userlogin} = require("../controllers/userControllers");
+const {addUser, userlogin, forgotPassword, resetPassword, emailVerification} = require("../controllers/userControllers");
 
 const router = express.Router();
 
 router.route("/reg").post(addUser);
 router.route("/login").post(userlogin);
+// router.route("/forgotPassword").post(forgotPassword);
+// router.route("/resetPassword").post(resetPassword);
+router.route("/verify-email/:id").get(emailVerification);
 
 
 module.exports = {
