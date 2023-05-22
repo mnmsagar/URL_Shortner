198dad471552bcd9382b9cf7dbe7c425b3d1de1b
Sagar <sagar785mishra@gmail.com>
2023-05-22 12:42:46 +0530

    refactor: deleted unused variables

--- connection.js
+++ connection.js
@@ -1,13 +1,17 @@
-const { MongoClient, Timestamp } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
 let dbConnection;
 let client;
 module.exports = {
-    connectToDb : async (dbUrl)=>{
-       client =  await MongoClient.connect(dbUrl);
-       console.log("Connected to Database");
-       dbConnection = client.db('url');
-    },
-    clientDB : ()=>{return client},  
-    getDb : ()=> {return dbConnection}
-}
+	connectToDb: async (dbUrl) => {
+		client = await MongoClient.connect(dbUrl);
+		console.log("Connected to Database");
+		dbConnection = client.db("url");
+	},
+	getClient: () => {
+		return client;
+	},
+	getDb: () => {
+		return dbConnection;
+	},
+};
