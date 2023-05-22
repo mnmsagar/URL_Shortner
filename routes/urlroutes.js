167ca752c9115f454ebd0410863a05747f8703ce
Sagar <sagar785mishra@gmail.com>
2023-05-22 20:23:13 +0530

    refactor: refactored code

--- routes/urlroutes.js
+++ routes/urlroutes.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const { addURL, getLink} = require("../controllers/urlControllers");
-const {checkUserAuth} = require("../auth-middleware");
+const { addURL, getLink } = require("../controllers/urlControllers");
+const { checkUserAuth } = require("../auth-middleware");
 
 const router = express.Router();
 
-router.use("/shorten",checkUserAuth);
+// router.use("/shorten", checkUserAuth);
 
 router.route("/:id").get(getLink);
-router.route("/shorten").post(addURL);
-
+router.route("/shorten").post(checkUserAuth, addURL);
 
 module.exports = {
-	router
+	router,
 };
