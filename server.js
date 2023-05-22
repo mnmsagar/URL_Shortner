6bb7f0d752b8196f39de442479927a18ef8c6845
Sagar <sagar785mishra@gmail.com>
2023-05-22 12:43:34 +0530

    refactor: deleted unused variables

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 const { port, app } = require("./app");
-const {connectToDb} = require('./connection')
+const { connectToDb } = require("./connection");
 
-connectToDb(process.env.URI).then(() => {
-	try {
-		app.listen(port, () => {
-			console.log(`Server is started at port - ${port}`);
-		})
-	} catch (error) {
-		console.log("Can't connect to the server!");
-	}
-}).catch((error) => {
-	console.log("Invalid Database Connection");
-})
+connectToDb(process.env.URI)
+	.then(() => {
+		try {
+			app.listen(port, () => {
+				console.log(`Server is started at port - ${port}`);
+			});
+		} catch (error) {
+			console.log("Can't connect to the server!");
+		}
+	})
+	.catch((error) => {
+		console.log(error);
+		console.log("Invalid Database Connection");
+	});
