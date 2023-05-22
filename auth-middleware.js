35bee6bffb467c3c4fd8019bda6ee243278ef457
Sagar <sagar785mishra@gmail.com>
2023-05-22 12:42:25 +0530

    refactor: deleted unused variables

--- auth-middleware.js
+++ auth-middleware.js
@@ -1,23 +1,22 @@
 const { auth } = require("./services/auth.dataHelper");
 
 exports.checkUserAuth = async (req, res, next) => {
-    const { authorization } = req.headers;
-    if (authorization && authorization.startsWith('Bearer')) {
-        try {
-            req.user = await auth(authorization);
-            // console.log(req.user);
-            next();
-        } catch (error) {
-            res.status(401).json({
-                mesaage: "Unauthorised User",
-                error : error
-            })
-        }
-    }
-    else {
-        res.status(401).json({
-            status: "Failed",
-            hint: "Token not sent, Unauthorised User"
-        })
-    }
-}
+	const { authorization } = req.headers;
+	if (authorization && authorization.startsWith("Bearer")) {
+		try {
+			req.user = await auth(authorization);
+			// console.log(req.user);
+			next();
+		} catch (error) {
+			res.status(401).json({
+				mesaage: "Unauthorised User",
+				error: error,
+			});
+		}
+	} else {
+		res.status(401).json({
+			status: "Failed",
+			hint: "Token not sent, Unauthorised User",
+		});
+	}
+};
