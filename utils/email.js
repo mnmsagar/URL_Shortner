ec7654a246f40495ad65863f270860228b0d24a1
Sagar <sagar785mishra@gmail.com>
2023-05-26 15:45:32 +0530

    fix: error handling added

--- utils/email.js
+++ utils/email.js
@@ -2,14 +2,13 @@ require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.API_KEY);
 
-exports.sendVerificationMail = (email, otp, name) => {
-	try {
-		sgMail.send({
-			to: email,
-			from: "sagar785mishra@gmail.com",
-			subject: "OTP Verification for Account Activation",
-			html: `<div class="container">
-					<p>Dear ${name},</p>
+exports.sendVerificationMail = async (email, otp) => {
+	const sendMail = await sgMail.send({
+		to: email,
+		from: "sagar785mishra@gmail.com",
+		subject: "OTP Verification for Account Activation",
+		html: `<div class="container">
+					<p>Dear User,</p>
 					<p>Please use the following OTP to verify your account:</p>
 					<h2 style="background-color: #f5f5f5; padding: 10px; font-weight: bold; font-size: 24px;">${otp}</h2>
 					<p>Note: This OTP is valid for a limited time and for a single use only.</p>
@@ -17,8 +16,10 @@ exports.sendVerificationMail = (email, otp, name) => {
 					<p>Best regards,</p>
 					<p>Your Company</p>
 				</div>`,
-		});
-	} catch (error) {
-		console.error(error);
+	});
+
+	if (!sendMail[0]) {
+		console.log("Hello");
+		throw new Error("Sending Mail Error");
 	}
 };
