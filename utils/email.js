4a140f7ce4b014478e10ba6ec8cf0aa1103bdb15
Sagar <sagar785mishra@gmail.com>
2023-05-27 11:59:07 +0530

    add: email sending using nodemailer added

--- utils/email.js
+++ utils/email.js
@@ -1,34 +1,54 @@
-/**
- *
- * Run:
- *
- */
 require("dotenv").config();
-const mailjet = require("node-mailjet").apiConnect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE);
-const request = mailjet.post("send", { version: "v3.1" }).request({
-	Messages: [
-		{
-			From: {
-				Email: "sagar785mishra@gmail.com",
-				Name: "Sagar",
-			},
-			To: [
-				{
-					Email: "sagar785mishra@gmail.com",
-					Name: "You",
-				},
-			],
-			Subject: "My first Mailjet Email!",
-			TextPart: "Greetings from Mailjet!",
-			HTMLPart:
-				"<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+const nodemailer = require("nodemailer");
+// const sgMail = require("@sendgrid/mail");
+// sgMail.setApiKey(process.env.API_KEY);
+
+// exports.sendVerificationMail = async (email, otp) => {
+// 	const sendMail = await sgMail.send({
+// 		to: email,
+// 		from: "sagar785mishra@gmail.com",
+// 		subject: "OTP Verification for Account Activation",
+// 		html: `<div class="container">
+// 					<p>Dear User,</p>
+// 					<p>Please use the following OTP to verify your account:</p>
+// 					<h2 style="background-color: #f5f5f5; padding: 10px; font-weight: bold; font-size: 24px;">${otp}</h2>
+// 					<p>Note: This OTP is valid for a limited time and for a single use only.</p>
+// 					<p>If you didn't request this verification, you can safely ignore this email.</p>
+// 					<p>Best regards,</p>
+// 					<p>Your Company</p>
+// 				</div>`,
+// 	});
+
+// 	if (!sendMail[0]) {
+// 		throw new Error("Sending Mail Error");
+// 	}
+// };
+
+exports.sendVerificationMail = async (email, otp, name) => {
+	let transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: "testoscompany@gmail.com",
+			pass: process.env.EMAIL_PASSWORD,
 		},
-	],
-});
-request
-	.then((result) => {
-		console.log(result.body);
-	})
-	.catch((err) => {
-		console.log(err.statusCode);
 	});
+
+	let mailOptions = {
+		from: "testoscompany@gmail.com",
+		to: email,
+		subject: "OTP Verification for Account Activation",
+		html: `<div class="container">
+					<p>Dear ${name},</p>
+					<p>Please use the following OTP to verify your account:</p>
+					<h2 style="background-color: #f5f5f5; padding: 10px; font-weight: bold; font-size: 24px;">${otp}</h2>
+					<p>Note: This OTP is valid for a limited time and for a single use only.</p>
+					<p>If you didn't request this verification, you can safely ignore this email.</p>
+					<p>Best regards,</p>
+					<p>Your Company</p>
+				</div>`,
+	};
+	const obj = await transporter.sendMail(mailOptions);
+	if (!obj.accepted.length) {
+		throw new Error("Mail Sending Error");
+	}
+};
