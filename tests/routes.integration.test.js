3cc59717543f1129ce30de76103895ca9a89ae3d
Sagar <sagar785mishra@gmail.com>
2023-05-27 23:50:09 +0530

    test: integration test implemented

--- tests/routes.integration.test.js
+++ tests/routes.integration.test.js
@@ -1,53 +1,82 @@
 require("dotenv").config();
 const request = require("supertest");
 const { app } = require("../app");
-const { beforeAll, afterAll } = require("@jest/globals");
-const { connectToDb, getDb, getClient } = require("../connection");
+const { beforeAll } = require("@jest/globals");
+const { connectToDb, getDb } = require("../connection");
+const { fetchData } = require("../utils/utils");
+const { generate } = require("otp-generator");
+const md5 = require("md5");
 
-const mockUserData = {
-	name: "Sagar Mishra",
-	email: "sagar785mishra@gmail.com",
-	password: "Sagar@123",
+const signUp = async (obj) => {
+	return await request(app).post("/users/signup").send(obj);
 };
 
-const mockUrl = {
-	url: "https://wwww.google.com",
+const loginUser = async (obj) => {
+	return await request(app).post("/users/login").send(obj);
 };
 
-// const postURL = async (token, body) => {
-//     const result = await request(app).post("/shorten")
-// }
+const verifyUser = async (obj) => {
+	return await request(app).post("/users/verify-email").send(obj);
+};
 
-describe("Tests", () => {
-	beforeAll(async () => {
-		await connectToDb(process.env.URI);
-	});
-	afterAll(async () => {
-		const db = getDb();
-		await db.collection("users").deleteMany({ email: "sagar785mishra@gmail.com" });
-		await db.collection("urlshortner").deleteMany({ longUrl: "https://www.google.com" });
-		await getClient().close();
+const addUrl = async (obj, token) => {
+	return await request(app).post("/shorten").set("Authorization", `Bearer ${token}`).send(obj);
+};
+
+const getObjById = async (id) => {
+	return request(app).get(`/${id}`);
+};
+
+beforeAll(async () => {
+	await connectToDb(process.env.URI);
+});
+
+describe("All tests", () => {
+	const mockUserData = {
+		name: "Sagar Mishra",
+		email: `${generate(7, {
+			upperCaseAlphabets: false,
+			specialChars: false,
+			digits: false,
+			lowerCaseAlphabets: true,
+		})}@amozix.com`,
+		password: "Sagar@123",
+	};
+	const hash = md5(mockUserData.email);
+	afterEach(async () => {
+		await getDb().collection("users").deleteOne({ email: mockUserData.email });
+		await getDb().collection("otp").deleteOne({ email: mockUserData.email });
+		await getDb().collection("urlshortner").deleteOne({ email: "https://www.google.com" });
 	});
 
-	it("When user tries to signUp it will return object", async () => {
-		const response = await request(app).post("/users/reg").send(mockUserData);
-		expect(response.body).toEqual(
+	test("When user signup and get OTP and verify it", async () => {
+		const result = await signUp(mockUserData);
+		expect(result.body).toEqual(
 			expect.objectContaining({
-				status: expect.any(String),
-				hint: expect.any(String),
-				token: expect.any(String),
+				message: expect.any(String),
 			})
 		);
-		expect(response.statusCode).toBe(201);
-	});
+		await new Promise((resolve) => setTimeout(resolve, 4000));
+		const otp = await fetchData(hash);
+		const otpObj = {
+			email: mockUserData.email,
+			otp: otp,
+		};
+		const response = await verifyUser(otpObj);
+		expect(response.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+	}, 8000);
 
-	it("When user sign up using either invalid name, email or password", async () => {
+	test("When user sign up using either invalid name, email or password", async () => {
 		const user1 = {
 			name: "Sagar1 Mis4shra",
 			email: "mishra@gmail.com",
 			password: "Sagar@123",
 		};
-		const respName = await request(app).post("/users/reg").send(user1);
+		const respName = await signUp(user1);
 		expect(respName.statusCode).toBe(400);
 
 		const user2 = {
@@ -55,7 +84,7 @@ describe("Tests", () => {
 			email: "mishra@gmai",
 			password: "Sagar@123",
 		};
-		const respEmail = await request(app).post("/users/reg").send(user2);
+		const respEmail = await signUp(user2);
 		expect(respEmail.statusCode).toBe(400);
 
 		const user3 = {
@@ -63,44 +92,248 @@ describe("Tests", () => {
 			email: "mishra@gmail.com",
 			password: "123456789",
 		};
-		const respPass = await request(app).post("/users/reg").send(user3);
+		const respPass = await await signUp(user3);
 		expect(respPass.statusCode).toBe(400);
 	});
 
-	it("when user tried to sign up with existing email", async () => {
-		const result = await request(app).post("/users/reg").send(mockUserData);
-		expect(result.statusCode).toBe(409);
-	});
-
-	it("When user tries to login with registered email", async () => {
-		const result = await request(app).post("/users/login").send(mockUserData);
-		expect(result.statusCode).toBe(200);
-	});
-
-	it("when user tries to login with non-registered email", async () => {
-		const user = {
-			name: "Sagar Mishra",
-			email: "sagar786mishra@gmail.com",
-			password: "123456789",
-		};
-		const response = await request(app).post("/users/login").send(user);
-		expect(response.statusCode).toBe(401);
-	});
-
-	it("When user enters incorrect password", async () => {
-		const user = {
-			name: "Sagar Mishra",
-			email: "sagar785mishra@gmail.com",
-			password: "1234567",
-		};
-		const response = await request(app).post("/users/login").send(user);
-		expect(response.statusCode).toBe(401);
-	});
-	it("When user tried to access post url enpoint using token", async () => {
-		const response = await request(app).post("/users/login").send(mockUserData);
-		const authToken = response.body.token;
-		const result = await request(app).post("/shorten").set("Authorization", `Bearer ${authToken}`).send(mockUrl);
+	test("when a user signup and verify and login and generate a token", async () => {
+		const result = await signUp(mockUserData);
 		expect(result.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+		await new Promise((resolve) => setTimeout(resolve, 4000));
+		const otp = await fetchData(hash);
+		const otpObj = {
+			email: mockUserData.email,
+			otp: otp,
+		};
+		const response = await verifyUser(otpObj);
+		expect(response.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+		const loginObj = {
+			email: mockUserData.email,
+			password: mockUserData.password,
+		};
+		const resp = await loginUser(loginObj);
+		expect(resp.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+				token: expect.any(String),
+			})
+		);
+	}, 8000);
+
+	test("when a user signs up and verify and login and generate token and then post a url", async () => {
+		const result = await signUp(mockUserData);
+		expect(result.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		const otp = await fetchData(hash);
+		const otpObj = {
+			email: mockUserData.email,
+			otp: otp,
+		};
+		const response = await verifyUser(otpObj);
+		expect(response.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+		const loginObj = {
+			email: mockUserData.email,
+			password: mockUserData.password,
+		};
+		const resp = await loginUser(loginObj);
+		expect(resp.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+				token: expect.any(String),
+			})
+		);
+		const authToken = resp.body.token;
+		const obj = {
+			url: "https://www.google.com",
+		};
+		const data = await addUrl(obj, authToken);
+		expect(data.body).toEqual(
+			expect.objectContaining({
+				urlCode: expect.any(String),
+				longUrl: expect.any(String),
+				shortUrl: expect.any(String),
+			})
+		);
+	}, 6000);
+});
+
+describe("Test for verify function", () => {
+	const mockUserData = {
+		name: "Sagar Mishra",
+		email: `${generate(7, {
+			upperCaseAlphabets: false,
+			specialChars: false,
+			digits: false,
+			lowerCaseAlphabets: true,
+		})}@amozix.com`,
+		password: "Sagar@123",
+	};
+	const hash = md5(mockUserData.email);
+
+	const loginCredentials = {
+		email: mockUserData.email,
+		password: mockUserData.password,
+	};
+
+	const mockUrl = {
+		url: "https://www.google.com",
+	};
+
+	afterAll(async () => {
+		getDb().collection("users").deleteOne({ email: mockUserData.email });
+		getDb().collection("otp").deleteOne({ email: mockUserData.email });
+	});
+	test("when user signup", async () => {
+		const result = await signUp(mockUserData);
+		expect(result.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+	});
+
+	test("when user try to verify with invalid otp or invalid email id or both", async () => {
+		const obj = {
+			email: mockUserData.email,
+			otp: generate(6, {
+				upperCaseAlphabets: false,
+				specialChars: false,
+				digits: true,
+				lowerCaseAlphabets: false,
+			}),
+		};
+		const verifiedObj = await verifyUser(obj);
+		expect(verifiedObj.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+	});
+
+	test("when user try to verify with empty body", async () => {
+		const obj = {};
+		const verifiedObj = await verifyUser(obj);
+		expect(verifiedObj.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+	});
+
+	test("when user try to verify before signup", async () => {
+		const obj = {
+			email: "sagar@gmail.com",
+			otp: "123456",
+		};
+		const verifiedObj = await verifyUser(obj);
+		expect(verifiedObj.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+	});
+
+	test("when user uses correct email and otp for verification", async () => {
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		const otp = await fetchData(hash);
+		const obj = {
+			email: mockUserData.email,
+			otp: otp,
+		};
+		const verifiedObj = await verifyUser(obj);
+		expect(verifiedObj.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+	}, 6000);
+
+	test("when already verified user tries to signup or verify", async () => {
+		const result = await signUp(mockUserData);
+		expect(result.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+		await new Promise((resolve) => setTimeout(resolve, 2000));
+		const otp = await fetchData(hash);
+		const obj = {
+			email: mockUserData.email,
+			otp: otp,
+		};
+		const verifiedObj = await verifyUser(obj);
+		expect(verifiedObj.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+	}, 6000);
+
+	test("when user try to login with correct credentials", async () => {
+		const resp = await loginUser(loginCredentials);
+		expect(resp.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+				token: expect.any(String),
+			})
+		);
+	});
+	test("when user try to login with incorrect credentials", async () => {
+		const loginCredentials = {
+			email: `${generate(8, {
+				upperCaseAlphabets: true,
+				specialChars: false,
+				digits: true,
+				lowerCaseAlphabets: true,
+			})}@amozix.com`,
+			password: generate(8, {
+				upperCaseAlphabets: true,
+				specialChars: true,
+				digits: true,
+				lowerCaseAlphabets: true,
+			}),
+		};
+		const resp = await loginUser(loginCredentials);
+		expect(resp.body).toEqual(
+			expect.objectContaining({
+				message: expect.any(String),
+			})
+		);
+		expect(resp.statusCode).toBe(401);
+	});
+
+	test("when user try to login with empty body", async () => {
+		const obj = {};
+		const resp = await loginUser(obj);
+		expect(resp.statusCode).toBe(400);
+	});
+
+	test("When user tried to access post url enpoint using token", async () => {
+		const resp = await loginUser(loginCredentials);
+		const authToken = resp.body.token;
+		const obj = {
+			url: "https://www.google.com",
+		};
+		console.log(mockUrl);
+		const data = await addUrl(mockUrl, authToken);
+		console.log(data.body);
+		expect(data.body).toEqual(
 			expect.objectContaining({
 				urlCode: expect.any(String),
 				longUrl: expect.any(String),
@@ -109,25 +342,25 @@ describe("Tests", () => {
 		);
 	});
 
-	it("when user tried to access post url endpoint using correct token and then access shortUrl", async () => {
-		const response = await request(app).post("/users/login").send(mockUserData);
-		const authToken = response.body.token;
-		const result = await request(app).post("/shorten").set("Authorization", `Bearer ${authToken}`).send(mockUrl);
-		const id = result.body.urlCode;
+	test("when user tried to access post url endpoint using correct token and then access shortUrl", async () => {
+		const resp = await loginUser(loginCredentials);
+		const authToken = resp.body.token;
+		const data = await addUrl(mockUrl, authToken);
+		const id = data.body.urlCode;
 		const get = await request(app).get(`/${id}`);
 		expect(get.statusCode).toBe(301);
 	});
 
-	it("When user login but uses incorrect token", async () => {
-		const response = await request(app).post("/users/login").send(mockUserData);
+	test("When user login but uses incorrect token", async () => {
+		const response = await loginUser(loginCredentials);
 		let authToken = response.body.token;
 		authToken = authToken.concat("Hello");
-		const result = await request(app).post("/shorten").set("Authorization", `Bearer ${authToken}`).send(mockUrl);
+		const result = await addUrl(mockUrl, authToken);
 		expect(result.statusCode).toBe(401);
 	});
 
-	it("When user login but no token used", async () => {
-		const response = await request(app).post("/users/login").send(mockUserData);
+	test("When user login but no token used in post /shorten", async () => {
+		const response = await loginUser(loginCredentials);
 		expect(response.body).toEqual(
 			expect.objectContaining({
 				message: expect.any(String),
@@ -135,30 +368,30 @@ describe("Tests", () => {
 			})
 		);
 		const authToken = "";
-		const result = await request(app).post("/shorten").set("Authorization", `Bearer ${authToken}`).send(mockUrl);
+		const result = await addUrl(mockUrl, authToken);
 		expect(result.statusCode).toBe(401);
 	});
 
-	it("When user login and generate token but in url post request enters an empty body", async () => {
-		const response = await request(app).post("/users/login").send(mockUserData);
+	test("When user login and generate token but in url post request enters an empty body", async () => {
+		const response = await loginUser(loginCredentials);
 		const authToken = response.body.token;
 		const obj = {};
-		const data = await request(app).post("/shorten").set("Authorization", `Bearer ${authToken}`).send(obj);
+		const data = await addUrl(obj, authToken);
 		expect(data.statusCode).toBe(400);
 	});
-	it("When user login and generate token but in url post request enters an invalid body", async () => {
-		const response = await request(app).post("/users/login").send(mockUserData);
+	test("When user login and generate token but in url post request enters an invalid body", async () => {
+		const response = await loginUser(loginCredentials);
 		const authToken = response.body.token;
 		const obj = {
 			url: "www.google.com",
 		};
-		const data = await request(app).post("/shorten").set("Authorization", `Bearer ${authToken}`).send(obj);
+		const data = await addUrl(obj, authToken);
 		expect(data.statusCode).toBe(400);
 	});
-	it("when user login and generate token and post the url correctly but in get request enter invalid id", async () => {
-		const response = await request(app).post("/users/login").send(mockUserData);
+	test("when user login and generate token and post the url correctly but in get request enter invalid id", async () => {
+		const response = await loginUser(loginCredentials);
 		const authToken = response.body.token;
-		const result = await request(app).post("/shorten").set("Authorization", `Bearer ${authToken}`).send(mockUrl);
+		const result = await addUrl(mockUrl, authToken);
 		expect(result.body).toEqual(
 			expect.objectContaining({
 				urlCode: expect.any(String),
@@ -167,14 +400,14 @@ describe("Tests", () => {
 			})
 		);
 		const id = "jiukdfbvf";
-		const data = await request(app).get(`/${id}`);
+		const data = await getObjById(id);
 		expect(data.statusCode).toBe(404);
 	});
 
-	it("when user login and generate token and post the url correctly but in get request enter empty id", async () => {
-		const response = await request(app).post("/users/login").send(mockUserData);
+	test("when user login and generate token and post the url correctly but in get request enter empty id", async () => {
+		const response = await loginUser(loginCredentials);
 		const authToken = response.body.token;
-		const result = await request(app).post("/shorten").set("Authorization", `Bearer ${authToken}`).send(mockUrl);
+		const result = await addUrl(mockUrl, authToken);
 		expect(result.body).toEqual(
 			expect.objectContaining({
 				urlCode: expect.any(String),
@@ -183,7 +416,7 @@ describe("Tests", () => {
 			})
 		);
 		const id = "";
-		const data = await request(app).get(`/${id}`);
+		const data = await getObjById(id);
 		expect(data.statusCode).toBe(404);
 	});
 });
