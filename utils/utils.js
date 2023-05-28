14c0559c45294429dd177df4eb39181d5b3527bf
Sagar <sagar785mishra@gmail.com>
2023-05-29 03:00:39 +0530

    refactor: code refactored for simplicity

--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,13 @@
 const { validate } = require("email-validator");
 const passwordValidator = require("password-validator");
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
-const otpGenerator = require("otp-generator");
+const { createHmac } = require("crypto");
+const { sign } = require("jsonwebtoken");
+const { generate } = require("otp-generator");
 
 const myHeaders = new Headers();
-myHeaders.append("apikey", "HxPQ1Yt9Kbe3I34XE8pUcsWfSi64DUxE");
+myHeaders.append("apikey", "nMO6iHmcwxvPl4MiDtjKbDuvKFJhFPC3");
 
-exports.isValidString = (str) => {
+const isValidString = (str) => {
 	if (str.trim().length === 0) {
 		return false;
 	}
@@ -33,28 +33,28 @@ schema
 	.not()
 	.spaces(); // Should not have spaces
 
-exports.isValidPassword = (password) => {
+const isValidPassword = (password) => {
 	return schema.validate(password);
 };
 
-exports.hashPassword = (password) => {
-	const hmac = crypto.createHmac("sha512", process.env.HASH_KEY);
+const hashPassword = (password) => {
+	const hmac = createHmac("sha512", process.env.HASH_KEY);
 	hmac.update(password);
 	const passwordHash = hmac.digest("hex");
 	return passwordHash;
 };
 
-exports.isValidEmail = (email) => {
+const isValidEmail = (email) => {
 	return validate(email);
 };
 
-exports.tokenGeneration = (obj) => {
-	const token = jwt.sign({ email: obj.email, userID: obj._id }, process.env.SECRET_KEY);
+const tokenGeneration = (obj) => {
+	const token = sign({ email: obj.email, userID: obj._id }, process.env.SECRET_KEY);
 	return token;
 };
 
-exports.otpGenerator = () => {
-	const generatedOTP = otpGenerator.generate(6, {
+const otpGenerator = () => {
+	const generatedOTP = generate(6, {
 		upperCaseAlphabets: false,
 		specialChars: false,
 		lowerCaseAlphabets: false,
@@ -62,35 +62,14 @@ exports.otpGenerator = () => {
 	return generatedOTP;
 };
 
-exports.badRequest = (message) => {
+const badRequest = (message) => {
 	return { message: message, statusCode: 400 };
 };
 
-exports.unAuthorisedReq = (message) => {
-	return {
-		message,
-		statusCode: 401,
-	};
-};
-
-exports.notFoundReq = (message) => {
-	return {
-		message,
-		statusCode: 404,
-	};
-};
-
-exports.createdReq = (message) => {
+const createdResp = (message) => {
 	return {
 		message,
 		statusCode: 201,
-	};
-};
-
-exports.okReq = (message) => {
-	return {
-		message,
-		statusCode: 200,
 	};
 };
 
@@ -116,7 +95,7 @@ const requestOptions = {
 	headers: myHeaders,
 };
 
-exports.fetchData = async (hashed_mail) => {
+const fetchData = async (hashed_mail) => {
 	try {
 		const response = await fetch(`https://api.apilayer.com/temp_mail/mail/id/${hashed_mail}`, requestOptions);
 		const result = await response.text();
@@ -126,4 +105,17 @@ exports.fetchData = async (hashed_mail) => {
 	} catch (error) {
 		console.log("error", error);
 	}
+};
+
+module.exports = {
+	findOTP,
+	fetchData,
+	createdResp,
+	badRequest,
+	isValidString,
+	isValidPassword,
+	otpGenerator,
+	tokenGeneration,
+	isValidEmail,
+	hashPassword,
 };
