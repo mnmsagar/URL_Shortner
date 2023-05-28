34508645ae5a2ccb2df7c68792860360a270fcce
Sagar <sagar785mishra@gmail.com>
2023-05-29 02:58:51 +0530

    refactor: code refactored for simplicity

--- services/auth.dataHelper.js
+++ services/auth.dataHelper.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-const jwt = require("jsonwebtoken");
+const { verify } = require("jsonwebtoken");
 const { getDb } = require("../connection");
 const { ObjectId } = require("mongodb");
 
 exports.auth = async (authorization) => {
 	const token = authorization.split(" ")[1];
-	const { userID } = jwt.verify(token, process.env.SECRET_KEY);
+	const { userID } = verify(token, process.env.SECRET_KEY);
 	const obj = await getDb()
 		.collection("users")
 		.findOne({ _id: new ObjectId(userID) });
