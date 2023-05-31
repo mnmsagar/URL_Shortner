36841d6d4f6e9f9657e6d65ea67a14c71e6c10b1
Sagar <sagar785mishra@gmail.com>
2023-05-31 14:40:14 +0530

    refactor: chnaged function name and removed options to use it dynamically

--- utils/email.js
+++ utils/email.js
@@ -24,7 +24,7 @@ const nodemailer = require("nodemailer");
 // 	}
 // };
 
-exports.sendVerificationMail = async (email, otp, name) => {
+exports.sendEmail = async (mailOptions) => {
 	let transporter = nodemailer.createTransport({
 		service: "gmail",
 		auth: {
@@ -33,20 +33,7 @@ exports.sendVerificationMail = async (email, otp, name) => {
 		},
 	});
 
-	let mailOptions = {
-		from: "testoscompany@gmail.com",
-		to: email,
-		subject: "OTP Verification for Account Activation",
-		html: `<div class="container">
-					<p>Dear ${name},</p>
-					<p>Please use the following OTP to verify your account:</p>
-					<h2 style="background-color: #f5f5f5; padding: 10px; font-weight: bold; font-size: 24px;">${otp}</h2>
-					<p>Note: This OTP is valid for a limited time and for a single use only.</p>
-					<p>If you didn't request this verification, you can safely ignore this email.</p>
-					<p>Best regards,</p>
-					<p>Your Company</p>
-				</div>`,
-	};
+	
 	const obj = await transporter.sendMail(mailOptions);
 	if (!obj.accepted.length) {
 		throw new Error("Mail Sending Error");
