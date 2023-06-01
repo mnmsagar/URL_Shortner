d9fb399ea4bca1d9a54a91fa9bc30c43acdabfc6
Sagar <sagar785mishra@gmail.com>
2023-06-01 11:06:36 +0530

    fix: fixed all functions

--- services/admin.dataHelper.js
+++ services/admin.dataHelper.js
@@ -6,14 +6,14 @@ const disableLoginHelper = async (body) => {
 	if (!isValidEmail(email)) {
 		return badRequest("Invalid email type!!");
 	}
-	const user = await getDb().collection("users").findOne({ email });
+	const user = await getDb().collection("users").findOne({ email: email, isAdmin: false, isLoginEnabled: true });
 	if (!user) {
-		return badRequest("user not exist!!");
+		return badRequest("Either user not registered or No enabled login users found !!");
 	}
 	const updatedObj = await getDb()
 		.collection("users")
 		.updateOne({ email }, { $set: { isLoginEnabled: false } });
-	if (!updatedObj) {
+	if (!updatedObj.modifiedCount || !updatedObj.matchedCount) {
 		throw new Error("Error in updation in disableLoginHelper");
 	}
 	return createdResp("user successfully disabled !!");
@@ -24,22 +24,42 @@ const deleteUserHelper = async (body) => {
 	if (!isValidEmail(email)) {
 		return badRequest("Invalid email type!!");
 	}
-	const user = await getDb().collection("users").findOne({ email });
+	const user = await getDb().collection("users").findOne({ email: email, isAdmin: false });
 	if (!user) {
 		return badRequest("user not exist!!");
 	}
 	const deletedUser = await getDb().collection("users").deleteOne({ email });
-	if (!deletedUser) {
+	console.log(deletedUser);
+	if (!deletedUser.deletedCount || !deletedUser.acknowledged) {
 		throw new Error("Error in deletion of User in deleteUserHelper");
 	}
 	const deletedUrls = await getDb().collection("urlshortner").deleteMany({ email });
-	if (!deletedUrls) {
+	if (!deletedUrls.deletedCount || !deletedUrls.acknowledged) {
 		throw new Error("Error in deletion of Urls in deleteUserHelper");
 	}
 	return createdResp("user successfully deleted !!");
 };
 
+const enableLoginHelper = async (body) => {
+	const { email } = body;
+	if (!isValidEmail(email)) {
+		return badRequest("Invalid email type!!");
+	}
+	const user = await getDb().collection("users").findOne({ email: email, isAdmin: false, isLoginEnabled: false });
+	if (!user) {
+		return badRequest("Either user not registered or No disabled login users found !!");
+	}
+	const updatedUser = await getDb()
+		.collection("users")
+		.updateOne({ email: email }, { $set: { isLoginEnabled: true } });
+	if (!updatedUser.modifiedCount || !updatedUser.matchedCount) {
+		throw new Error("Error in user updation in enableLoginHelper");
+	}
+	createdResp("User login enabled successfully!!");
+};
+
 module.exports = {
 	disableLoginHelper,
 	deleteUserHelper,
+	enableLoginHelper,
 };
