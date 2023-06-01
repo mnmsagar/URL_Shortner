b9486605cd7415fa1e2f2e55f6997adf46936711
Sagar <sagar785mishra@gmail.com>
2023-06-01 13:19:35 +0530

    add: added isLoginEnabled middleware on accessing of /shorten post req

--- routes/urlroutes.js
+++ routes/urlroutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { addURL, getLink } = require("../controllers/urlControllers");
 const { checkUserAuth } = require("../auth-middleware");
+const { isLoginEnabled } = require("../isLoginEnabled.middleware");
 
 const router = express.Router();
 
 router.use("/shorten", checkUserAuth);
 router.route("/:id").get(getLink);
-router.route("/shorten").post(addURL);
+router.route("/shorten").post(checkUserAuth, isLoginEnabled, addURL);
 
 module.exports = {
 	router,
