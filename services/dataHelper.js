0950154d1f06dd22b5090aa3a0b30953b07905ca
Sagar <sagar785mishra@gmail.com>
2023-06-01 11:30:28 +0530

    add: added email as argument in writeToDb

--- services/dataHelper.js
+++ services/dataHelper.js
@@ -4,18 +4,20 @@ const isUrlHttp = require("is-url-http");
 const { getDb } = require("../connection");
 
 module.exports = {
-	writeToDb: async (url) => {
+	writeToDb: async (url, email) => {
 		const id = randomstring.generate(8);
 		const obj = {
 			urlCode: id,
 			longUrl: url,
 			shortUrl: `http://localhost:${process.env.PORT || 3000}/${id}`,
+			email: email,
 		};
 		const insertData = await getDb().collection("urlshortner").insertOne(obj);
 		if (!insertData.acknowledged) {
 			throw new Error("Insertion Failed in URL writing in Db");
 		}
 		delete obj._id;
+		delete obj.email;
 		return obj;
 	},
 	getObjById: async (id) => {
