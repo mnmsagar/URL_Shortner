3a6a7996a4fffd176af3e12f922f8bb12de3bb12
Sagar <sagar785mishra@gmail.com>
2023-06-01 11:05:25 +0530

    add: enableLogin added

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { disableLogin, deleteUser } = require("../controllers/adminControllers");
+const { disableLogin, deleteUser, enableLogin } = require("../controllers/adminControllers");
 const { checkUserAuth } = require("../auth-middleware");
 const { isAdmin } = require("../isAdmin.middleware");
 
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.route("/disable-login").post(checkUserAuth, isAdmin, disableLogin);
 router.route("/delete-user").post(checkUserAuth, isAdmin, deleteUser);
+router.route("/enable-login").post(checkUserAuth, isAdmin, enableLogin);
 
 module.exports = {
 	router,
