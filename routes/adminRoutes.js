61f5367d70f9006b982adafba5bab201aeecc405
Sagar <sagar785mishra@gmail.com>
2023-06-02 10:21:33 +0530

    add: isValid middleware added for validations for email

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const { disableLogin, deleteUser, enableLogin } = require("../controllers/adminControllers");
 const { checkUserAuth } = require("../auth-middleware");
 const { isAdmin } = require("../isAdmin.middleware");
+const { isValid } = require("../isValid.middleware");
 
 const router = express.Router();
 
-router.route("/disable-login").post(checkUserAuth, isAdmin, disableLogin);
-router.route("/delete-user").post(checkUserAuth, isAdmin, deleteUser);
-router.route("/enable-login").post(checkUserAuth, isAdmin, enableLogin);
+router.route("/disable-login").post(checkUserAuth, isAdmin, isValid, disableLogin);
+router.route("/delete-user").post(checkUserAuth, isAdmin, isValid, deleteUser);
+router.route("/enable-login").post(checkUserAuth, isAdmin, isValid, enableLogin);
 
 module.exports = {
 	router,
