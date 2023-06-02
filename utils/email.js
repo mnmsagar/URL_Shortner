2187849d2e42c1d2c9107a49b7f45302a99911d4
Sagar <sagar785mishra@gmail.com>
2023-06-02 10:45:45 +0530

    fix: functions added for all email templates req

--- utils/email.js
+++ utils/email.js
@@ -24,7 +24,7 @@ const nodemailer = require("nodemailer");
 // 	}
 // };
 
-exports.sendEmail = async (mailOptions) => {
+const sendEmail = async (mailOptions) => {
 	let transporter = nodemailer.createTransport({
 		service: "gmail",
 		auth: {
@@ -33,9 +33,63 @@ exports.sendEmail = async (mailOptions) => {
 		},
 	});
 
-	
 	const obj = await transporter.sendMail(mailOptions);
 	if (!obj.accepted.length) {
 		throw new Error("Mail Sending Error");
 	}
+};
+
+exports.sendVerificationEmail = async (name, email, otp) => {
+	let mailOptions = {
+		from: "testoscompany@gmail.com",
+		to: email,
+		subject: "OTP Verification for Account Activation",
+		html: `<div class="container">
+					<p>Dear ${name},</p>
+					<p>Please use the following OTP to verify your account:</p>
+					<h2 style="background-color: #f5f5f5; padding: 10px; font-weight: bold; font-size: 24px;">${otp}</h2>
+					<p>Note: This OTP is valid for a limited time and for a single use only.</p>
+					<p>If you didn't request this verification, you can safely ignore this email.</p>
+					<p>Best regards,</p>
+					<p>Your Company</p>
+				</div>`,
+	};
+	await sendEmail(mailOptions);
+};
+
+exports.sendForgetPasswordEmail = async (name, email, otp) => {
+	let mailOptions = {
+		from: "testoscompany@gmail.com",
+		to: email,
+		subject: "OTP for Account's Password Reset",
+		html: `<div class="container">
+					<p>Dear ${name},</p>
+					<p>We received a request to reset your password for your account.</p>
+					<p>Please use the following OTP to reset your account:</p>
+					<h2 style="background-color: #f5f5f5; padding: 10px; font-weight: bold; font-size: 24px;">${otp}</h2>
+					<p>Note: This OTP is valid for a limited time and for a single use only.</p>
+					<p>If you didn't request this, you can safely ignore this email.</p>
+					<p>Best regards,</p>
+					<p>Your Company</p>
+				</div>`,
+	};
+	await sendEmail(mailOptions);
+};
+
+exports.sendResendOtpMail = async (name, email, otp) => {
+	let mailOptions = {
+		from: "testoscompany@gmail.com",
+		to: email,
+		subject: "New OTP for Verification",
+		html: `<div class="container">
+					<p>Dear ${name},</p>
+					<p>We noticed that you requested a new OTP to verify your account.</p>
+					<h2 style="background-color: #f5f5f5; padding: 10px; font-weight: bold; font-size: 24px;">${otp}</h2>
+					<p>Note: This OTP is valid for a limited time and for a single use only.</p>
+					<p>If you didn't request this, you can safely ignore this email.</p>
+					<p>Best regards,</p>
+					<p>Your Company</p>
+				</div>`,
+	};
+	await sendEmail(mailOptions);
 };
