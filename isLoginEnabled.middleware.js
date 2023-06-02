5c491bec71f807af137a7d88e517c6ef978b8a6d
Sagar <sagar785mishra@gmail.com>
2023-06-02 22:15:13 +0530

    fix: database call added, user will get checked from database now

--- isLoginEnabled.middleware.js
+++ isLoginEnabled.middleware.js
@@ -1,5 +1,7 @@
+const { findUser } = require("./services/user.dataHelper");
 exports.isLoginEnabled = async (req, res, next) => {
-	if (!req.user.isLoginEnabled) {
+	const user = await findUser({ email: req.user.email, isLoginEnabled: true });
+	if (!user) {
 		res.status(400).json({
 			status: "failed",
 			message: "you are disabled by admin, pls contact administrator!!",
