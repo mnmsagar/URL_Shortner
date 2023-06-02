0f6d61d186ebc722dd952c15977174994172a0fa
Sagar <sagar785mishra@gmail.com>
2023-06-02 10:10:00 +0530

    add: added userCheck

--- isAdmin.middleware.js
+++ isAdmin.middleware.js
@@ -1,4 +1,11 @@
+
 exports.isAdmin = async (req, res, next) => {
+	if (!req.user) {
+		res.status(400).json({
+			message: "Login to access this route!!",
+		});
+		return;
+	}
 	if (!req.user.isAdmin) {
 		res.status(400).json({
 			message: "You are not admin !",
